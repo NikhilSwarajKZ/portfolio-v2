5901de08e4c20a50d7b357b417b1a650e2b14d70
NikhilSwarajOfficial <nikhilswaraj.official@gmail.com>
2024-06-02 15:00:18 +0530

    Fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,12 +56,6 @@ module.exports = {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     {
-      resolve: 'gatsby-plugin-google-gtag',
-      options: {
-        trackingIds: ['G-R4EKTLHVCE'],
-      },
-    },
-    {
       resolve: `gatsby-plugin-canonical-urls`,
       options: {
         siteUrl: `https://nikhilswaraj.dev`,
